ee231bed1a139c5540cef6cf05e829e907c14144
jinsoobok <soobok.jin@deliveryhero.co.kr>
2021-05-23 12:10:17 +0900

    get private envs from .env

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,48 @@
 import { HardhatUserConfig } from "hardhat/types";
 import "@nomiclabs/hardhat-waffle";
+import { readFileSync } from 'fs';
+
+let getPrivateEnv: Function = function (filePath: string): Map<String, String> {
+    let env = readFileSync(filePath, 'utf-8');
+
+    let splitEnv: Array<string> = env.split(/\n/);
+    let envMapper = new Map();
+    for (let l in splitEnv) {
+        let values = splitEnv[l].split("=");
+        envMapper.set(values[0], values[1]);
+    }
+    return envMapper;
+}
+
+let envMappers: Map<String, String> = getPrivateEnv('./.env');
+
+const ROPSTEN_INFURA_KEY = envMappers.get('ROPSTEN_INFURA_KEY');
+const ROPSTEN_PRIVATE_KEY = envMappers.get('ROPSTEN_PRIVATE_KEY');
+
+
 const config: HardhatUserConfig = {
   solidity: {
     compilers: [{ version: "0.8.0", settings: {} }],
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    hardhat: {
+    },
+    ropsten: {
+      url: `https://ropsten.infura.io/v3/${ROPSTEN_INFURA_KEY}`,
+      accounts: [`${ROPSTEN_PRIVATE_KEY}`],
+    },
+  },
+  paths: {
+    sources: './contracts',
+    artifacts: './build/artifacts',
+    cache: './build/cache',
+    tests: './test',
   },
 };
 export default config;
